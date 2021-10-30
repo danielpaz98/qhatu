0fef4ed0793d404026bc302335579e5cedccb987
danielpaz98 <alexdaniel-1998@outlook.com>
2021-10-30 16:12:21 -0500

    Add auth page component

---
+++ src/pages/AuthPage/index.jsx
@@ -0,0 +1,12 @@
+// STYLES
+import "./styles/auth-page.css";
+// COMPONENTS
+import AuthForm from "~/components/AuthForm";
+
+export default function AuthPage() {
+	return (
+		<div className="auth-page">
+			<AuthForm />
+		</div>
+	);
+}
